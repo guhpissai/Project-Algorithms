8dc7b54901c6af5651a92b828a01784e6b940d09
guhpissai <gustavopissai3d@gmail.com>
2023-08-18 17:08:29 -0300

    Refatorando study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,23 +1,13 @@
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-
+# def is_valid_permanence_period(hour_1, hour_2)
 def study_schedule(permanence_period, target_time):
-    if target_time == None:
+    if target_time is None:
         return None
-    if not isinstance(permanence_period, list):
-        return None
-    contador = 0
+    estudantes = 0
     for hour in permanence_period:
         if not (isinstance(hour[0], int) and isinstance(hour[1], int)):
             return None
-        if hour[0] == None or hour[1] == None:
-            return None
-        if hour[0] == hour[1]:
-            time = [hour[0], hour[1]]
-        else:
-            time = list(range(hour[0], hour[1] + 1))
+        time = list(range(hour[0], hour[1] + 1))
         if target_time in time:
-            contador += 1
-    return contador
+            estudantes += 1
+    return estudantes
     raise NotImplementedError
-
-print(study_schedule(permanence_period, 2))
