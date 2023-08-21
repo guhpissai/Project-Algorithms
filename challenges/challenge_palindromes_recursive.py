c2bac179b0d11f85ddd2da8587b797c40081cfb4
guhpissai <gustavopissai3d@gmail.com>
2023-08-21 16:08:40 -0300

    Requisito 3 - testando se uma palavra e um palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if not word:
+        return False
+    if word[low_index] == word[high_index]:
+        try:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        except IndexError:
+            return True
+    else:
+        return False
     raise NotImplementedError
