6b58ab2762ad9011eb20343fed88fca5b4699abd
guhpissai <gustavopissai3d@gmail.com>
2023-08-21 20:34:08 -0300

    Requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,23 @@
+def order_string(string):
+    if len(string) <= 1:
+        return string
+
+    pivot = string[len(string) // 2]
+    left = ''.join([char for char in string if char < pivot])
+    middle = ''.join([char for char in string if char == pivot])
+    right = ''.join([char for char in string if char > pivot])
+
+    return order_string(left) + middle + order_string(right)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if not first_string or not second_string:
+        return tuple([first_string, second_string, False])
+
+    first_ordered = order_string(first_string)
+    second_ordered = order_string(second_string)
+
+    return tuple([
+        first_ordered, second_ordered, first_ordered == second_ordered])
+
     raise NotImplementedError
-
-
-"""first commit"""
