a6d702a955231fd608fe1c3e999fdab940b7eb1c
guhpissai <gustavopissai3d@gmail.com>
2023-08-21 17:20:07 -0300

    Implementando testes para a funcao encrypt_message - Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('xablau', 3) == 'bax_ual'
+    assert encrypt_message('xablau', 4) == 'ua_lbax'
+    assert encrypt_message('xablau', 99) == 'ualbax'
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('xablau', 'xablau')
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(5, 2)
