eba6984d8f290a4316ce023036293b54184f6342
guhpissai <gustavopissai3d@gmail.com>
2023-08-28 19:41:06 -0300

    Implementando o requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,16 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    
+    n = len(word)
+    bol = list()
+
+    for index in range(n):
+        if word[index] == word[len(word) - index - 1]:
+            bol.append(True)
+        else:
+            bol.append(False)
+
+    result = set(bol)
+    
+    return not False in result
